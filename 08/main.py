a441ec01a178f03b5f6873ec6bf036859f94ac73
CA2528357431 <2528357431@QQ.com>
2021-07-20 23:10:50 +0800

    normal

--- 08/main.py
+++ 08/main.py
@@ -1,52 +1,49 @@
-import  collections
+import collections
 
 #
 
-#namedtuple
-a=('x', 'y','r')
+# namedtuple
+a = ('x', 'y', 'r')
 circle = collections.namedtuple('CIRCLE', a)
-p = circle(1, 2,3)#创建
+p = circle(1, 2, 3)  # 创建
 print(p)
 print(p.r)
-#为元组命名使我们可以用名称来引用数据
+# 为元组命名使我们可以用名称来引用数据
 # 同时规定内部数据个数
 # 相当于一个mini类
 
-#deque
-b=['a', 'b', 'c']
+# deque
+b = ['a', 'b', 'c']
 q = collections.deque(b)
 q.appendleft('xxx')
 print(list(q))
 q.popleft()
 print(list(q))
-#使我们可以高效的在开头添加数据
+# 使我们可以高效的在开头添加/删除数据
 
-#defaultdict
+# defaultdict
 r = collections.defaultdict(lambda: 'NONONONONONONONNO')
-r['0']=976
+r['0'] = 976
 print(r['0'])
 print(r['1'])
-#访问未定义的key时返回给出的结果
-#可惜的属性是只能后续添加
+# 访问未定义的key时返回给出的结果
+# 可惜的属性是只能后续添加
 
-#OrderedDict
-od = collections.OrderedDict({'1':11,'2':22,'3':33})
-od['0']=00
-od['1']=111111
-od['4']=44
+# OrderedDict
+od = collections.OrderedDict({'1': 11, '2': 22, '3': 33})
+od['0'] = 00
+od['1'] = 111111
+od['4'] = 44
 print(dict(od))
-listofod=list(od.keys())
-#按插入顺序返回key
+listofod = list(od.keys())
+# 按插入顺序返回key
 for x in listofod:
-    print(od[x],end='   ')
+    print(od[x], end='   ')
 print()
-#顺序返回value
+# 顺序返回value
 
-#计数器
-co_base=[1,1,1,2,3,1,2,'a','A',True,True,None,None,False,False,'asfdafd']
+# 计数器
+co_base = [1, 1, 1, 2, 3, 1, 2, 'a', 'A', True, True, None, None, False, False, 'asfdafd']
 co = collections.Counter(co_base)
 print(dict(co))
-#true记为1，false记为0
-
-
-
+# true记为1，false记为0
